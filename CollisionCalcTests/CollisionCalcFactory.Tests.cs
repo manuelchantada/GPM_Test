31cf2415b3d5237240d7437cb38b31515822bd8f
manuelchantada <manuelchantada@hotmail.com>
2019-06-22 18:17:17 -0300

    Add CollisionCalcFactory.Tests

---
+++ CollisionCalcTests/CollisionCalcFactory.Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+
+
+namespace CollisionCalcTests
+{
+    [TestClass]
+    public class CubesFactoryCreatesCubeCollisionCalc
+    {
+        readonly Cube A = new Cube(new Coordinate(1, 1, 1), 2);
+        readonly Cube B = new Cube(new Coordinate(3, 3, 3), 5);
+
+        [TestMethod]
+        public void GivenTwoCubesFactoryCreatesCubeCollisionCalc()
+        {
+            CollisionCalc calc = CollisionCalcFactory.CreateCollisionCalc(A, B);
+            Assert.IsNotNull(calc);
+            Assert.IsInstanceOfType(calc, typeof(CubeCollisionCalc) );
+        }
+
+
+    }
+}
